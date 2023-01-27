8c6de6fb61cfb5fdbd876a2021ab016cf3b7106f
Osomhe1 <osomhe.aleogho290@gmail.com>
2023-01-27 23:21:32 +0100

    homepage responsiveness

--- components/Appreance.js
+++ components/Appreance.js
@@ -10,6 +10,7 @@ import { Typography } from '@mui/material'
 import { useState } from 'react'
 import  api from '../pages/api/darlink'
 import { APPREANCE } from '../pages/api/ACTIONS.JS'
+import { toast } from 'react-toastify'
 
 
 
---
+++ components/admin_components/DeleteModal.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import { Button } from '@mui/material'
+// import { USER } from '../pages/api/ACTIONS.JS'
+// import api from '../pages/api/darlink'
+import api from '../../pages/api/darlink'
+import { toast } from 'react-toastify'
+import { GET_USERS } from '../../pages/api/ACTIONS.JS'
+import DeleteIcon from '@mui/icons-material/Delete'
+
+
+export default function DeleteModal() {
+  const [showModal, setShowModal] = useState(false)
+
+
+   const handleDeleteAcc = async () => {
+    setShowModal(false)
+     try {
+       const { data } = await api.get(GET_USERS.DELETE_ACC(), {})
+       if (data.success)
+         //todo
+         //populate UI
+         // setUser(data.profile)
+         toast.success('user successful deleted')
+     } catch (error) {
+       // console.log(error.response.data.error)
+       console.log(error, 'error')
+       toast.error(error.response.data.error)
+       // if (error.response.status === 401) {
+       //   Logout()
+       // }
+     }
+   }
+
+  return (
+    <>
+      <button
+        className=" text-red-500 active:bg-pink-600 
+         font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg 
+         outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        <DeleteIcon /> 
+        Delete
+      </button>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-[99999] shadow-8xl outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-md">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-4xl relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <p className="p-6 flex-auto m-auto">Confirm delete account</p>
+                <div className="flex items-center justify-between p-6 border-t border-solid border-slate-200 rounded-b">
+                  <Button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => handleDeleteAcc()}
+                    name="yes"
+                    value="yes"
+                  >
+                    Yes
+                  </Button>
+                  <Button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                    name="no"
+                    value="no"
+                  >
+                    No
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  )
+}
